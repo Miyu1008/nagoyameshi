b9a613fa2e92d8b51aa68969e18cd9a3782a0f5e
Miyu1008 <yamori304@gmail.com>
2024-04-23 18:28:23 +0900

    iroiro

--- kadai_002/myproject/crud/models.py
+++ kadai_002/myproject/crud/models.py
@@ -4,38 +4,6 @@ from django.utils import timezone
 from datetime import datetime
 from django.conf import settings
 
-class User(AbstractUser):
-    email = models.EmailField('メールアドレス', unique=True)
-    is_store = models.BooleanField(default=False)
-
-    # groupsフィールドとuser_permissionsフィールドをUserクラス内に移動し、'to'引数を追加
-    groups = models.ManyToManyField(
-        Group,  # 'to'引数にGroupモデルを指定
-        verbose_name='groups',
-        blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-        related_name="crud_user_groups",  # related_nameを追加
-        related_query_name="user",
-        # through='UserGroups'  # 中間モデルの指定が必要な場合にはコメントを解除してください
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,  # 'to'引数にPermissionモデルを指定
-        verbose_name='user permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_name="crud_user_permissions",  # related_nameを追加
-        related_query_name="user",
-        # through='UserUserPermissions'  # 中間モデルの指定が必要な場合にはコメントを解除してください
-    )
-"""
-class CustomUser(AbstractUser):
-    # ここにカスタムフィールドを追加できます
-    age = models.PositiveIntegerField(null=True, blank=True)
-    address = models.CharField(max_length=255, null=True, blank=True)
-
-    def __str__(self):
-        return self.username
-        """
     
 class Store(models.Model):
     name = models.CharField(max_length=200)
